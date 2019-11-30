281367ab299cc05ac1d01c4a2877666481af655c
pupysheva <anime-manga@bk.ru>
2019-11-30 16:26:04 +0300

    Backend 1 версия

---
+++ src/main/java/ru/mirea/ItemService/BackJDBCTemplate.java
@@ -0,0 +1,36 @@
+package ru.mirea.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import java.sql.ResultSet;
+import java.util.List;
+
+@Component
+public class BackJDBCTemplate {
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public BackJDBCTemplate(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Item> geItems() {
+        System.out.println("jdbcTemplate");
+        return jdbcTemplate.query("select * from Item", (ResultSet resultSet, int rowNum) -> {
+            return new Item(resultSet.getInt("id"), resultSet.getString("name"), resultSet.getString("type"), resultSet.getInt("count"), resultSet.getDouble("price"));
+        });
+    }
+
+    public List<Item> deleteItem(int id) {
+        try {
+            jdbcTemplate.update("DELETE FROM Cart WHERE id=?",id);
+        }catch(DataAccessException dataAccessException){
+        }
+
+        return jdbcTemplate.query("select * from Item", (ResultSet resultSet, int rowNum) -> {
+            return new Item(resultSet.getInt("id"), resultSet.getString("name"), resultSet.getString("type"), resultSet.getInt("count"), resultSet.getDouble("price"));
+        });
+    }
+}
---
+++ src/main/java/ru/mirea/ItemService/Resp.java
@@ -0,0 +1,42 @@
+package ru.mirea.ItemService;
+
+import java.util.List;
+
+public class Resp {
+    private String method;
+    private boolean status;
+    private List<Item> item;
+
+    public Resp(String method, boolean status, List<Item> item) {
+        this.method = method;
+        this.status = status;
+        this.item = item;
+    }
+
+    public Resp() {
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public List<Item> getBook() {
+        return item;
+    }
+
+    public void setBook(List<Item> item) {
+        this.item = item;
+    }
+}
