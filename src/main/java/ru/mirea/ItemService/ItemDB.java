1aa0e5914b293feb3afa741f27ac62f97efec045
pupysheva <anime-manga@bk.ru>
2019-12-08 23:35:13 +0300

    Исправлен файл ItemDB

--- src/main/java/ru/mirea/ItemService/ItemDB.java
+++ src/main/java/ru/mirea/ItemService/ItemDB.java
@@ -15,11 +15,11 @@ public class ItemDB {
 
     public void init(){
         // init db инициализация базы данных
-        jdbcTemplate.execute("CREATE TABLE IF NOT EXISTS Item(id int NOT NULL PRIMARY KEY ,name VARCHAR , type VARCHAR,count int, price DOUBLE )");
+        jdbcTemplate.execute("CREATE TABLE IF NOT EXISTS Item(id int NOT NULL PRIMARY KEY AUTO_INCREMENT ,name VARCHAR , type VARCHAR,count int, price DOUBLE )");
 
         //в базе данных цена в долларах
         //наполняем базу тоже тут
-        jdbcTemplate.execute("INSERT INTO Item( id, name,type,count,price) VALUES (1, 'dog', 'pet',3, 15), ( 2,'cat', 'pet',3,13),( 3,'cat food', 'stuff',4,10),( 4,'dog food', 'stuff',3,10)");
+        jdbcTemplate.execute("INSERT INTO Item( name,type,count,price) VALUES ('dog', 'pet',3, 15), ('cat', 'pet',3,13),('cat food', 'stuff',4,10),('dog food', 'stuff',3,10)");
 
 
     }
