35ff6f100875c693ced6d0e6cb22aae03220207f
pupysheva <anime-manga@bk.ru>
2020-03-24 01:46:54 +0300

    add ControllerTest

---
+++ src/test/java/ControllerTest.java
@@ -0,0 +1,42 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.mirea.ItemService.ItemController;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.core.StringContains.containsString;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ControllerTest {
+
+    @Autowired
+    private ItemController itemController;
+
+    @Test
+    public void test() throws Exception {
+        assertThat(itemController).isNotNull();
+    }
+}
+//@SpringBootTest
+//@AutoConfigureMockMvc
+//public class ControllerTest {
+//
+//    @Autowired
+//    private MockMvc mockMvc;
+//
+//    @Test
+//    public void shouldReturnDefaultMessage() throws Exception {
+//        this.mockMvc.perform(get("/"))
+//                .andDo(print())
+//                .andExpect(status().isOk())
+//                .andExpect(content().string(containsString("Hello, World")));
+//    }
+//}
