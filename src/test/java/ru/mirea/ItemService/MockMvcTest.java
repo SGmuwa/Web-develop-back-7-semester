bde2fca6282ffcb3f5401a6dfe7564e64e16424c
SG_Muwa <motherlode.muwa@gmail.com>
2020-03-25 10:00:47 +0300

    MockMvcTest.java: Delete unused comments

--- src/test/java/ru/mirea/ItemService/MockMvcTest.java
+++ src/test/java/ru/mirea/ItemService/MockMvcTest.java
@@ -40,21 +40,3 @@ public class MockMvcTest {
                 .andExpect(content().string(containsString("Hello, Mock123")));
     }
 }
-
-
-
-//@SpringBootTest
-//@AutoConfigureMockMvc
-//public class ControllerTest {
-//
-//    @Autowired
-//    private MockMvc mockMvc;
-//
-//    @Test
-//    public void shouldReturnDefaultMessage() throws Exception {
-//        this.mockMvc.perform(get("/"))
-//                .andDo(print())
-//                .andExpect(status().isOk())
-//                .andExpect(content().string(containsString("Hello, World")));
-//    }
-//}
