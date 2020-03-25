eb4a25de5364d0b07f8bc69dad538ef920c1d832
SG_Muwa <motherlode.muwa@gmail.com>
2020-03-25 10:21:04 +0300

    MockMvcTest: "@BeforeAll" → "@BeforeEach", "public void" → "void"

--- src/test/java/ru/mirea/ItemService/MockMvcTest.java
+++ src/test/java/ru/mirea/ItemService/MockMvcTest.java
@@ -1,10 +1,12 @@
 package ru.mirea.ItemService;
-import org.junit.jupiter.api.BeforeAll;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+
 import static org.hamcrest.Matchers.containsString;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -13,7 +15,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(ItemController.class)
-public class MockMvcTest {
+class MockMvcTest {
 
     @Autowired
     private MockMvc mockMvc;
@@ -21,14 +23,13 @@ public class MockMvcTest {
     @MockBean
     private ItemService service;
 
-    @BeforeAll
-    public void init()
-    {
+    @BeforeEach
+    void init() {
         when(service.greet()).thenReturn("Hello, Mock123");
     }
 
     @Test
-    public void test2() throws Exception {
+    void test2() throws Exception {
         this.mockMvc.perform(get("/greeting"))
                 .andDo(print())
                 .andExpect(status().isOk())
