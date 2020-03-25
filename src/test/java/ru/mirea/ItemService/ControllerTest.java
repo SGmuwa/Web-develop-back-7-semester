ed865991cb9809e0adfe65661fd7bbe5cde49feb
pupysheva <anime-manga@bk.ru>
2020-03-25 15:15:39 +0300

    add deleteItemByIdTest

--- src/test/java/ru/mirea/ItemService/ControllerTest.java
+++ src/test/java/ru/mirea/ItemService/ControllerTest.java
@@ -8,12 +8,16 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -52,5 +56,44 @@ class ControllerTest {
                 .andExpect(status().isOk());
 
         assertTrue(isItemInList[0]);
+    }
+    @Test
+    void deleteItemByIdTest() throws Exception {
+        final boolean[] isItemInList = {true};
+        doAnswer(invocationOnMock -> isItemInList[0] = false)
+                .when(service)
+                .deleteItem(1);
+
+        this.mockMvc.perform(
+                delete("/1")
+                        .content(JSONValue.toJSONString(
+                                new Item(1,"dog", "pet", 3, 15.0)))
+                        .characterEncoding("UTF-8")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        assertFalse(isItemInList[0]);
+    }
+    @Test
+    public void findByIdTest() throws Exception {
+        Item item = new Item(1, "dog", "pet", 3, 15);
+        given(service.getById(1)).willReturn(item);
+        this.mockMvc.perform(get("/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(JSONValue.toJSONString(item)));
+    }
+
+    @Test
+    public void findAllTest() throws Exception {
+        Item item1 = new Item(1, "dog", "pet", 3, 15);
+        Item item2 = new Item(2, "cat", "pet", 3, 13);
+        Item item3 = new Item(3, "cat food", "stuff", 4, 10);
+        Item item4 = new Item(4, "dog food", "stuff", 3, 10);
+        List<Item> itemList = Arrays.asList(item1, item2, item3, item4);
+        given(service.geItems()).willReturn(itemList);
+
+        this.mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(JSONValue.toJSONString(itemList)));
     }
 }
