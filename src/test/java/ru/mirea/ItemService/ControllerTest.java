773e007e6c02ff4d845e51deca293e5d4cad275e
SG_Muwa <motherlode.muwa@gmail.com>
2020-03-25 12:58:49 +0300

    Increase java version

--- src/test/java/ru/mirea/ItemService/ControllerTest.java
+++ src/test/java/ru/mirea/ItemService/ControllerTest.java
@@ -2,14 +2,18 @@ package ru.mirea.ItemService;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -23,16 +27,26 @@ class ControllerGreetingTest {
     @MockBean
     private ItemService service;
 
-    @BeforeEach
-    void init() {
-        when(service.greet()).thenReturn("Hello, Mock123");
-    }
-
     @Test
     void greetingTest() throws Exception {
+        when(service.greet()).thenReturn("Hello, Mock123");
         this.mockMvc.perform(get("/greeting"))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().string(containsString("Hello, Mock123")));
+    }
+
+    @Test
+    void putItemTest() throws Exception {
+        final boolean[] isItemInList = {false};
+        doAnswer(invocationOnMock -> isItemInList[0] = true)
+                .when(service)
+                .putItem("1", "2", 3, 4.0);
+
+        this.mockMvc.perform(
+                put("/", new Item("2", "3", 4, 5.0)))
+                .andExpect(status().isOk());
+
+        assertTrue(isItemInList[0]);
     }
 }
