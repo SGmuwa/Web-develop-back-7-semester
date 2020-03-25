c8ece4f01ae9c11396d047f1efad12375cf5ebf3
SG_Muwa <motherlode.muwa@gmail.com>
2020-03-25 14:51:30 +0300

    Fix set public for JSON

--- src/main/java/ru/mirea/ItemService/Item.java
+++ src/main/java/ru/mirea/ItemService/Item.java
@@ -8,15 +8,15 @@ class Item {
     private double price;
 
 
-    double getPrice() {
+    public double getPrice() {
         return price;
     }
 
-    void setPrice(double price) {
+    public void setPrice(double price) {
         this.price = price;
     }
 
-    Item(int id, String name, String type, int count, double price) {
+    public Item(int id, String name, String type, int count, double price) {
         this.id = id;
         this.name = name;
         this.type = type;
@@ -24,42 +24,42 @@ class Item {
         this.price = price;
     }
 
-    Item(String name, String type, int count, double price) {
+    public Item(String name, String type, int count, double price) {
         this.name = name;
         this.type = type;
         this.count = count;
         this.price = price;
     }
 
-    int getId() {
+    public int getId() {
         return id;
     }
 
-    String getType() {
+    public String getType() {
         return type;
     }
 
-    int getCount() {
+    public int getCount() {
         return count;
     }
 
-    String getName() {
+    public String getName() {
         return name;
     }
 
-    void setId(int id) {
+    public void setId(int id) {
         this.id = id;
     }
 
-    void setName(String name) {
+    public void setName(String name) {
         this.name = name;
     }
 
-    void setType(String type) {
+    public void setType(String type) {
         this.type = type;
     }
 
-    void setCount(int count) {
+    public void setCount(int count) {
         this.count = count;
     }
 }
